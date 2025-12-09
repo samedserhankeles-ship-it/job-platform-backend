14e9118d1da2797431f23d9890bea9016e97ef52
samedserhankeles-ship-it <samedserhankeles@gmail.com>
2025-12-09 23:17:53 +0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.models import AbstractUser
+ffrom django.contrib.auth.models import AbstractUser
 from django.db import models
 import uuid
 
 class User(AbstractUser):
-    # username alanını devre dışı bırakıyoruz, çünkü login'de email kullanacağız.
     username = None 
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -15,23 +14,15 @@ class User(AbstractUser):
 
 class Job(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    
-    # Kullanıcı silinirse, ilanlar da silinir (CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
-    
     title = models.CharField(max_length=200)
     description = models.TextField()
     
-    # Performans için db_index=True eklendi
-    location_city = models.CharField(max_length=100, db_index=True) 
-    
-    # Performans için db_index=True eklendi (Sıralama için kritik)
-    created_at = models.DateTimeField(auto_now_add=True, db_index=True) 
+    location_city = models.CharField(max_length=100, db_index=True) # İndeks eklendi
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True) # İndeks eklendi
 
     def __str__(self):
         return self.title
 
     class Meta:
-        # Django veritabanında Job modelinin birden fazla alana göre 
-        # nasıl sıralanacağını belirler. En yeniler başta olur.
         ordering = ['-created_at']

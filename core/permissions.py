f1f9587d17d5d27ea7b3c35f5cd06be0d516f1a6
samedserhankeles-ship-it <samedserhankeles@gmail.com>
2025-12-09 23:18:21 +0300

    Update permissions.py

--- core/permissions.py
+++ core/permissions.py
@@ -2,16 +2,10 @@
 from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Sadece objenin sahibi yazma (PUT, PATCH, DELETE) iznine sahiptir.
-    Okuma (GET) herkese açıktır.
-    """
+    """Sadece objenin sahibi yazma/düzenleme/silme iznine sahiptir."""
     
     def has_object_permission(self, request, view, obj):
-        # Güvenli metotlar (GET, HEAD, OPTIONS) okuma işlemleridir ve herkese izin verilir.
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # Diğer metotlar (PUT, PATCH, DELETE) yazma işlemleridir.
-        # Bu izin, isteği yapan kullanıcının (request.user) objenin (Job ilanı) sahibi (obj.user) olup olmadığını kontrol eder.
         return obj.user == request.user
